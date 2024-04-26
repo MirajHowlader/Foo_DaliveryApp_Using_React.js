e583e65452e0ae7532d386e3be29d3700563f03e
miraj <mirajhowlader9999@gmail.com>
2024-04-27 00:08:07 +0600

    first commit

---
+++ src/components/explore-menu/ExploreMenu.jsx
@@ -0,0 +1,27 @@
+
+
+import './exploreMenu.css'
+import { menu_list } from '../../assets/assets'
+const ExploreMenu = ({category,setCategory}) => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Food is more than mere sustenance; it's a cultural cornerstone, a source of joy, and a means of connection. Across the globe, culinary traditions vary widely, reflecting the diverse landscapes, histories, and preferences of different regions and peoples.</p>
+      
+      <div className="explore-menu-list">
+        {menu_list.map((item, index)=>{
+            return(
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                  <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                  <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
+
+    </div>
+  )
+}
+
+export default ExploreMenu
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Food is more than mere sustenance; it's a cultural cornerstone, a source of joy, and a means of connection. Across the globe, culinary traditions vary widely, reflecting the diverse landscapes, histories, and preferences of different regions and peoples.</p>
+
+         <button>View Menu</button>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
